ba96234e2790ef2c98503f0b4843a012bc72a371
Andres Reyes <karlosareyes@gmail.com>
2018-08-30 04:16:32 -0500

    Avance 2 Formulario Front de Registro

--- wp-content/themes/asquimco-theme/t_register_us.php
+++ wp-content/themes/asquimco-theme/t_register_us.php
@@ -5,6 +5,16 @@ Template Name: Pagina de Registro
 get_header( ); ?>
 
 <div class="container">
+	<?php
+		if ( isset($_GET['sent']) ){
+			if ( $_GET['sent'] == '1'){
+				echo "<p> ✔ Solicitud enviada correctamente</p><br>";
+			}
+			else {
+				echo "<p>  Hubo un error al enviar la solicitud</p><br>";
+			}
+		}
+	?>
 	<div class="col-md-2 col-lg-2 col-sm-2">
 		<?php dynamic_sidebar('barra'); ?>
 	</div>
@@ -27,14 +37,17 @@ get_header( ); ?>
 				<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
 			</div>
 			<div class="form-group">
-				<label for="exampleInputPassword1">Password</label>
-				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				<label for="type-membership">Tipo de Membresía</label>
+				<select class="form-control" id="type-membership" name="type-membership">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+				</select>
 			</div>
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1">
-				<label class="form-check-label" for="exampleCheck1">Check me out</label>
-			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
+			<input type="hidden" name="action" value="process_form">
+			<button type="submit" class="btn btn-primary">Enviar</button>
 		</form>
 		<p>&nbsp;</p>
 	</div>
